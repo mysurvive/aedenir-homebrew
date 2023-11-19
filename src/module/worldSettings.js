16f52938c9d38a8765826a2424d0409316268d0f
Ricky <rckruse91@gmail.com>
2023-11-19 14:47:12 -0500

    Updates for 5.9.x

--- src/module/worldSettings.js
+++ src/module/worldSettings.js
@@ -1,44 +1,44 @@
 const creatureTraits = [
-  { id: "hb_vexbalg", value: "Vexbalg" },
-  { id: "hb_drakkgebor", value: "Drakkgebor" },
-  { id: "hb_oropin", value: "Oropin" },
+  { id: "vexbalg", value: "Vexbalg" },
+  { id: "drakkgebor", value: "Drakkgebor" },
+  { id: "oropin", value: "Oropin" },
 ];
 
 const featTraits = [
-  { id: "hb_vexbalg", value: "Vexbalg" },
-  { id: "hb_drakkgebor", value: "Drakkgebor" },
-  { id: "hb_dragon", value: "Dragon" },
-  { id: "hb_oropin", value: "Oropin" },
+  { id: "vexbalg", value: "Vexbalg" },
+  { id: "drakkgebor", value: "Drakkgebor" },
+  { id: "dragon", value: "Dragon" },
+  { id: "oropin", value: "Oropin" },
 ];
 
 const languages = [
-  { id: "hb_skin-cant", value: "Skin Cant" },
-  { id: "hb_oropin", value: "Oropin" },
-  { id: "hb_ekkdu", value: "Ekkdu" },
-  { id: "hb_isserian", value: "Isserian" },
-  { id: "hb_hetzlati", value: "Hetz'lati" },
-  { id: "hb_ekkaril", value: "Ekkaril" },
-  { id: "hb_liluathan", value: "Liluathan" },
-  { id: "hb_skree", value: "Skree" },
-  { id: "hb_oropini", value: "Oropini" },
-  { id: "hb_sultic", value: "Sultic" },
-  { id: "hb_bastaandi", value: "Bastaandi" },
-  { id: "hb_dockspeak", value: "Dockspeak" },
-  { id: "hb_patchwerk", value: "Patchwerk" },
-  { id: "hb_tripup", value: "Tripup" },
+  { id: "skin-cant", value: "Skin Cant" },
+  { id: "oropin", value: "Oropin" },
+  { id: "ekkdu", value: "Ekkdu" },
+  { id: "isserian", value: "Isserian" },
+  { id: "hetzlati", value: "Hetz'lati" },
+  { id: "ekkaril", value: "Ekkaril" },
+  { id: "liluathan", value: "Liluathan" },
+  { id: "skree", value: "Skree" },
+  { id: "oropini", value: "Oropini" },
+  { id: "sultic", value: "Sultic" },
+  { id: "bastaandi", value: "Bastaandi" },
+  { id: "dockspeak", value: "Dockspeak" },
+  { id: "patchwerk", value: "Patchwerk" },
+  { id: "tripup", value: "Tripup" },
 ];
 
-const weaponGroups = [{ id: "hb_oropin", value: "Oropin" }];
+const weaponGroups = [{ id: "oropin", value: "Oropin" }];
 
 const baseWeapons = [
-  { id: "hb_rock", value: "Rock" },
-  { id: "hb_oropin-greathammer", value: "Oropin Greathammer" },
-  { id: "hb_oropin-greatsword", value: "Oropin Greatsword" },
-  { id: "hb_oropin-composite-longbow", value: "Oropin Composite Longbow" },
-  { id: "hb_oropin-composite-shortbow", value: "Oropin Composite Shortbow" },
+  { id: "rock", value: "Rock" },
+  { id: "oropin-greathammer", value: "Oropin Greathammer" },
+  { id: "oropin-greatsword", value: "Oropin Greatsword" },
+  { id: "oropin-composite-longbow", value: "Oropin Composite Longbow" },
+  { id: "oropin-composite-shortbow", value: "Oropin Composite Shortbow" },
 ];
 
-const weaponTraits = [{ id: "hb_oropin", value: "Oropin" }];
+const weaponTraits = [{ id: "oropin", value: "Oropin" }];
 
 export {
   creatureTraits,
