4e3d88809554db4b5bff5d1fdba47a2b4ac5aa02
Ricky <rckruse91@gmail.com>
2026-06-15 10:06:50 -0400

    argumentative handlebars helper

--- src/module/config.js
+++ src/module/config.js
@@ -1,28 +1,40 @@
 export function registerHandlebarsHelpers() {
-  Handlebars.registerHelper("when", function (op1, operator, op2, opts) {
-    const operators = {
-        eq: function (l, r) {
-          return l == r;
+  Handlebars.registerHelper(
+    "when",
+    function (this, op1, operator, op2, opts) {
+      const operators = {
+          eq: function (l, r) {
+            return l === r;
+          },
+          noteq: function (l, r) {
+            return l !== r;
+          },
+          gt: function (l, r) {
+            return l > r;
+          },
+          gte: function (l, r) {
+            return l >= r;
+          },
+          lt: function (l, r) {
+            return l < r;
+          },
+          lte: function (l, r) {
+            return l <= r;
+          },
+          or: function (l, r) {
+            return l || r;
+          },
+          and: function (l, r) {
+            return l && r;
+          },
+          "%": function (l, r) {
+            return l % r === 0;
+          },
         },
-        noteq: function (l, r) {
-          return l != r;
-        },
-        gt: function (l, r) {
-          return Number(l) > Number(r);
-        },
-        or: function (l, r) {
-          return l || r;
-        },
-        and: function (l, r) {
-          return l && r;
-        },
-        "%": function (l, r) {
-          return l % r === 0;
-        },
-      },
-      result = operators[operator](op1, op2);
+        result = operators[operator](op1, op2);
 
-    if (result) return opts.fn(this);
-    else return opts.inverse(this);
-  });
+      if (result) return opts.fn(this);
+      else return opts.inverse(this);
+    },
+  );
 }
